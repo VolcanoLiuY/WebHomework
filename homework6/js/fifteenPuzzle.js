8e6f7f097fa5b0d09eec6c5c7ff6aa79c5d997a4
VolcanoLiuY <654966197@qq.com>
2016-10-22 20:34:49 +0800

    homework6-version1.0

--- homework6/js/fifteenPuzzle.js
+++ homework6/js/fifteenPuzzle.js
@@ -10,51 +10,6 @@ window.onload = function() {
     document.getElementById('start').onclick = game;
 }
 
-function game() {
-    var i, j;
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            var m = i*4+j+1;
-            var p = document.getElementById("p"+m);
-            if(p) {//Cannot read property 'addEventListener' of null
-                console.log("a");
-                p.addEventListener('click', Sliding);
-            }
-        }
-    }
-    randomOrder();
-}
-
-function isFinish() {
-    for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 4; j++) {
-            if (picPosition[i][j] != 4*i+j+1) return;
-        }
-    }
-    alert("CONGRATULATIONS!");
-}
-
-function randomOrder() {
-    
-    var i, j;
-    var random, temp;
-    for (i = 14; i >= 0; i--) {
-        random = Math.round(Math.random()*i);
-        tmp = picPosition[Math.floor(i/4)][i%4];
-        picPosition[Math.floor(i/4)][i%4] = picPosition[Math.floor(random/4)][random%4];
-        picPosition[Math.floor(random/4)][random%4] = tmp;
-    }
-
-    var pic = document.getElementsByClassName("pic");
-    for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 4; j++) {
-            if (!(i == 3 && j == 3)) {
-                // console.log(picPosition[i][j]);
-                pic[4*i+j].id = "p"+picPosition[i][j];
-            }
-        }
-    }
-}
 
 function Sliding(e) {
     var posY, posX;
@@ -93,4 +48,53 @@ function Sliding(e) {
     }
     isFinish();
 }
+
+function game() {
+    var i, j;
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            var m = i*4+j+1;
+            var p = document.getElementById("p"+m);
+            if(p) {//Cannot read property 'addEventListener' of null
+                console.log("a");
+                p.addEventListener('click', Sliding);
+            }
+        }
+    }
+    randomOrder();
+}
+
+
+
+function isFinish() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (picPosition[i][j] != 4*i+j+1) return;
+        }
+    }
+    alert("CONGRATULATIONS!");
+}
+
+function randomOrder() {
+    
+    var i, j;
+    var random, temp;
+    for (i = 14; i >= 0; i--) {
+        random = Math.round(Math.random()*i);
+        tmp = picPosition[Math.floor(i/4)][i%4];
+        picPosition[Math.floor(i/4)][i%4] = picPosition[Math.floor(random/4)][random%4];
+        picPosition[Math.floor(random/4)][random%4] = tmp;
+    }
+
+    var pic = document.getElementsByClassName("pic");
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (!(i == 3 && j == 3)) {
+                // console.log(picPosition[i][j]);
+                pic[4*i+j].id = "p"+picPosition[i][j];
+            }
+        }
+    }
+}
+
 
